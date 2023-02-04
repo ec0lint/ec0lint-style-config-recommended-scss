4df3db0e89c3c03215e8a1e880a1c91d1094278c
Anna Gut <annagut026@gmail.com>
2023-02-04 18:38:54 +0100

    Fix formatting and tests

--- __tests__/index.js
+++ __tests__/index.js
@@ -2,7 +2,7 @@
 
 const config = require('../');
 const fs = require('fs');
-const stylelint = require('ec0lint-style');
+const ec0lint = require('ec0lint-style');
 
 const validScss = fs.readFileSync('./__tests__/valid.scss', 'utf-8');
 const invalidScss = fs.readFileSync('./__tests__/invalid.scss', 'utf-8');
@@ -40,39 +40,39 @@ describe('flags warnings with invalid scss', () => {
 		return result.then((data) => expect(data.errored).toBeTruthy());
 	});
 
-	it('flags two warnings', () => {
-		return result.then((data) => expect(data.results[0].warnings).toHaveLength(2));
+	it('flags three warnings', () => {
+		return result.then((data) => expect(data.results[0].warnings).toHaveLength(3));
 	});
 
 	it('correct warning text no-ttf-font-files', () => {
 		return result.then((data) =>
 			expect(data.results[0].warnings[0].text).toBe(
-        'Format of the custom font can be changed to WOFF or WOFF2. CO2 reduction: up to 80% of the font file.\n' +
-        'Your file can be converted online at https://cloudconvert.com/\n' +
-        'Estimated CO2 reduction that you can achieve by converting your file is: 0.01g (no-ttf-font-files)',
-        ),
+				'Format of the custom font can be changed to WOFF or WOFF2. ' +
+					'Your file can be converted online at https://cloudconvert.com/\n' +
+					'Estimated CO2 reduction that you can achieve by converting your file is: 0.05g (no-ttf-font-files)',
+			),
 		);
 	});
 
-  it('correct warning text require-font-display', () => {
-    return result.then((data) =>
-      expect(data.results[0].warnings[1].text).toBe(
-        'No font-display property specified inside @font-face rule. (require-font-display)',
-      ),
-    );
-  });
+	it('correct warning text require-font-display', () => {
+		return result.then((data) =>
+			expect(data.results[0].warnings[1].text).toBe(
+				'No font-display property specified inside @font-face rule. (require-font-display)',
+			),
+		);
+	});
 
-  it('correct rule flagged no-ttf-font-files', () => {
+	it('correct rule flagged no-ttf-font-files', () => {
 		return result.then((data) =>
 			expect(data.results[0].warnings[0].rule).toBe('no-ttf-font-files'),
 		);
 	});
 
-  it('correct rule flagged require-font-display', () => {
-    return result.then((data) =>
-      expect(data.results[0].warnings[1].rule).toBe('require-font-display'),
-    );
-  });
+	it('correct rule flagged require-font-display', () => {
+		return result.then((data) =>
+			expect(data.results[0].warnings[1].rule).toBe('require-font-display'),
+		);
+	});
 
 	it('correct severity flagged', () => {
 		return result.then((data) => expect(data.results[0].warnings[0].severity).toBe('error'));
